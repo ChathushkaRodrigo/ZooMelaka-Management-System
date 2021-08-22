73a471b3a1760298d4f478c0c450f9fe90b22b27
Shasvathan <pshasvathan1999@gmail.com>
2021-08-22 08:31:48 +0530

    All CRUD APIS

--- Shasvathan/routes/posts.js
+++ Shasvathan/routes/posts.js
@@ -20,5 +20,52 @@ router.post('/post/save',(req,res)=>{
 
 });
 
+router.get('/posts',(req,res)=>{
+
+    Posts.find().exec((err,posts) => {
+        if(err){
+            return res.status(400).json({
+                error:err
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            existingPosts:posts
+        })
+    });
+});
+
+router.put('/post/update/:id',(req,res) =>{
+    Posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+
+            return res.status(200).json({
+                success:"Updated Succesfully"
+            });
+        }
+        
+    );
+});
+
+router.delete('/post/delete/:id',(req,res) =>{
+    Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost) =>{
+       
+        if(err) return res.staus(400).json({
+            message:"Delete unsuccessful",err
+        });
+        
+        return res.json({
+            message:"Delete Succesfull",deletedPost
+        });
+    }); 
+});
+
 
 module.exports = router;
