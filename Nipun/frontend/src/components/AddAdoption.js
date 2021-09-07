af4f2f0a770c902833b57e9689f6c324bd299b4e
nipunbandara <nipun.bandara@outlook.com>
2021-09-07 23:54:43 +0530

    frontend added

--- Nipun/frontend/src/components/AddAdoption.js
+++ Nipun/frontend/src/components/AddAdoption.js
@@ -1,82 +1,120 @@
-import React, {useState} from 'react';
 import axios from 'axios';
+import React, { Component } from 'react';
 
+class AddAdoption extends Component {
 
-export default function AddAdoption(){
-    
-    let [animal_name, setAnimalName] = useState("");
-    let [adoption_level, setAdoptionLevel] = useState("");
-    let [payment_plan, setPaymentPlan] = useState("");
-    let [live_cam, setLiveCam] = useState("");
-    let [adoption_date, setAdoptionDate] = useState("");
-    let [animal_id, setAnimalId] = useState("");
-    let [member_id, setMemberId] = useState("");
+    constructor(props){
+        super(props);
+        this.setAnimalName = this.setAnimalName.bind(this);
+        this.setAdoptionLevel = this.setAdoptionLevel.bind(this);
+        this.setPaymentPlan = this.setPaymentPlan.bind(this);
+        this.setLiveCam = this.setLiveCam.bind(this);
+        this.setAdoptionDate = this.setAdoptionDate.bind(this);
+        this.setAnimalId = this.setAnimalId.bind(this);
+        this.setMemberId = this.setMemberId.bind(this);
+        this.saveAdoption = this.saveAdoption.bind(this);
 
-    function sendData(e){
-        //e - event, because sendData is an event
-        e.preventDefault(); //form submit default is to send data to another site. to prevent default behavior this is used
+        this.state = {
+            animal_name: '',
+            adoption_level: '',
+            payment_plan: '',
+            live_cam: '',
+            adoption_date: '',
+            animal_id: '',
+            member_id: ''
 
-        const newEmployee = {
-            animal_name,
-            adoption_level,
-            payment_plan,
-            live_cam,
-            adoption_date,
-            animal_id,
-            member_id
         }
-       
-        axios.post('http://localhost:8070/adoption/add', newEmployee)
-        .then(()=>{
-            alert('Adoption Record Added');
-
-            setAnimalName("");
-            setAdoptionLevel();
-            setPaymentPlan("");
-            setLiveCam("");
-
-        })
-        .catch((err) =>{
-            alert(err);
-        });
-
-        //can use this on button as onClick or on form as onSubmit
     }
-    
-    return(
 
-        <div className = "container">
-        <form onSubmit = {sendData}>
-            <div className="form-group">
-                <label for="name">Animal Name</label>
-                <input type="text" className="form-control" id="name" placeholder="Enter name" onChange = {(e) => {
-                    setAnimalName(e.target.value); //assigning filled value on input field to state variable name
-                }}/>
-                
+
+    setAnimalName(e){
+        this.setState({animal_name: e.target.value});
+    }
+    setAdoptionLevel(e){
+        this.setState({adoption_level : e.target.value});
+    }
+    setPaymentPlan(e){
+        this.setState({payment_plan : e.target.value});
+    }
+    setLiveCam(e){
+        this.setState({live_cam : e.target.value});
+    }
+    setAdoptionDate(e){
+        this.setState({adoption_date : e.target.value});
+    }
+    setAnimalId(e){
+        this.setState({animal_id : e.target.value});
+    }
+    setMemberId(e){
+        this.setState({member_id : e.target.value});
+    }
+
+    saveAdoption(e){
+        e.preventDefault();
+        
+        console.log('Adoption data', this.state);
+
+        const adoption = {
+            animal_name : this.state.animal_name,
+            adoption_level: this.state.adoption_level,
+            payment_plan: this.state.payment_plan,
+            live_cam: this.state.live_cam,
+            adoption_date: this.state.adoption_date,
+            animal_id: this.state.animal_id,
+            member_id: this.state.member_id
+        }
+        axios.post("/adoption/add", adoption).then(() => {
+            alert('adoption report added');
+        })
+        .catch(error => {
+            alert (error.message);
+        });
+    }
+
+    render() {
+        return (
+            <div className = "container">
+            
+                <div className="form-group">
+                    <label for="name">Animal Name</label>
+                    <input type="text" className="form-control" id="name" placeholder="Enter Animal name" value = {this.state.animal_name} onChange = {this.setAnimalName}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Adoption Level</label>
+                    <input type="number" className="form-control" id="age" placeholder="Enter Adoption Level" value = {this.state.adoption_level} onChange = {this.setAdoptionLevel}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="name">Payment Plan</label>
+                    <input type="text" className="form-control" id="gender" placeholder="Enter Payment Plan" value = {this.state.payment_plan} onChange = {this.setPaymentPlan}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Live Cam</label>
+                    <input type="number" className="form-control" id="age" placeholder="Enter Live Cam" value = {this.state.live_cam} onChange = {this.setLiveCam}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Adoption Date</label>
+                    <input type="number" className="form-control" id="age" placeholder="Enter Adoption Date" value = {this.state.adoption_date} onChange = {this.setAdoptionDate}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Animal Id</label>
+                    <input type="number" className="form-control" id="age" placeholder="Enter Animal Id" value = {this.state.animal_id} onChange = {this.setAnimalId}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Member Id</label>
+                    <input type="number" className="form-control" id="age" placeholder="Enter Memeber Id" value = {this.state.member_id} onChange = {this.setMemberId}/>
+                    
+                </div>
+                  <button className="btn btn-primary" onClick = {this.saveAdoption}>Submit</button>
+            
             </div>
-            <div className="form-group">
-                <label for="age">Adoption Leve</label>
-                <input type="text" className="form-control" id="age" placeholder="Enter age" onChange = {(e) => {
-                    setAdoptionLevel(e.target.value); 
-                }}/>
-                
-            </div>
-            <div className="form-group">
-                <label for="name">Payment Plan</label>
-                <input type="text" className="form-control" id="gender" placeholder="Enter gender" onChange = {(e) => {
-                    setPaymentPlan(e.target.value); 
-                }}/>
-                
-            </div>
-            <div className="form-group">
-                <label for="name">Live Cam</label>
-                <input type="text" className="form-control" id="gender" placeholder="Enter gender" onChange = {(e) => {
-                    setLiveCam(e.target.value); 
-                }}/>
-                
-            </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        </div>
-        )
+        );
+    }
 }
+
+export default AddAdoption;
--- Nipun/frontend/src/components/UpdateAdoption.js
+++ Nipun/frontend/src/components/UpdateAdoption.js
@@ -1,37 +1,140 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-export default function updateAdoption(){
-     
-    
-    
-    return(
-        <div className = "container">
-        <form>
-            <div className="form-group">
-                <label for="name">Name</label>
-                <input type="text" className="form-control" id="name" placeholder="Enter name" onChange = {(e) => {
-                    //setName(e.target.value); //assigning filled value on input field to state variable name
-                }}/>
-                
-            </div>
-            <div className="form-group">
-                <label for="age">Age</label>
-                <input type="number" className="form-control" id="age" placeholder="Enter age" onChange = {(e) => {
-                    //setAge(e.target.value); 
-                }}/>
-                
-            </div>
-            <div className="form-group">
-                <label for="name">Gender</label>
-                <input type="text" className="form-control" id="gender" placeholder="Enter gender" onChange = {(e) => {
-                    //setGender(e.target.value); 
-                }}/>
-                
-            </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        </div>
-        )
+class UpdateAdoption extends Component {
 
+    constructor(props){
+        super(props);
+        this.setAnimalName = this.setAnimalName.bind(this);
+        this.setAdoptionLevel = this.setAdoptionLevel.bind(this);
+        this.setPaymentPlan = this.setPaymentPlan.bind(this);
+        this.setLiveCam = this.setLiveCam.bind(this);
+        this.setAdoptionDate = this.setAdoptionDate.bind(this);
+        this.setAnimalId = this.setAnimalId.bind(this);
+        this.setMemberId = this.setMemberId.bind(this);
+        this.saveAdoption = this.saveAdoption.bind(this);
+        this.state = {
+            animal_name: '',
+            adoption_level: '',
+            payment_plan: '',
+            live_cam: '',
+            adoption_date: '',
+            animal_id: '',
+            member_id: ''
+
+        }
+    }
+
+
+    setAnimalName(e){
+        this.setState({animal_name: e.target.value});
+    }
+    setAdoptionLevel(e){
+        this.setState({adoption_level : e.target.value});
+    }
+    setPaymentPlan(e){
+        this.setState({payment_plan : e.target.value});
+    }
+    setLiveCam(e){
+        this.setState({live_cam : e.target.value});
+    }
+    setAdoptionDate(e){
+        this.setState({adoption_date : e.target.value});
+    }
+    setAnimalId(e){
+        this.setState({animal_id : e.target.value});
+    }
+    setMemberId(e){
+        this.setState({member_id : e.target.value});
+    }
+    saveAdoption(e){
+        e.preventDefault();
+        const id = this.props.match.params.id;
+        
+        console.log('Employee data', this.state);
+
+        const adoption = {
+            animal_name : this.state.animal_name,
+            adoption_level: this.state.adoption_level,
+            payment_plan: this.state.payment_plan,
+            live_cam: this.state.live_cam,
+            adoption_date: this.state.adoption_date,
+            animal_id: this.state.animal_id,
+            member_id: this.state.member_id
+        }
+        axios.put(`/adoption/update/${id}`, adoption).then(() => {
+            alert('Adoption Record updated');
+        })
+        .catch(error => {
+            alert (error.message);
+        });
+    }
+
+
+    componentDidMount() {
+        const id = this.props.match.params.id;
+
+        axios.get(`/adoption/get/${id}`).then((res) => {
+            if (res.data.success){
+                this.setState({
+                    animal_name: res.data.adoption.animal_name,
+                    animal_name: res.data.adoption.animal_name,
+                    adoption_level: res.data.adoption.animal_name,
+                    payment_plan: res.data.adoption.animal_name,
+                    live_cam: res.data.adoption.animal_name,
+                    adoption_date: res.data.adoption.animal_name,
+                    animal_id: res.data.adoption.animal_name,
+                    member_id: res.data.adoption.animal_name
+                });
+
+                console.log(res.data.adoption);
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className = "container">
+            <div className="form-group">
+                    <label for="name">Animal Name</label>
+                    <input type="text" className="form-control" id="name"  value = {this.state.animal_name} onChange = {this.setAnimalName}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Adoption Level</label>
+                    <input type="number" className="form-control" id="age" value = {this.state.adoption_level} onChange = {this.setAdoptionLevel}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="name">Payment Plan</label>
+                    <input type="text" className="form-control" id="gender"  value = {this.state.payment_plan} onChange = {this.setPaymentPlan}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Live Cam</label>
+                    <input type="number" className="form-control" id="age"  value = {this.state.live_cam} onChange = {this.setLiveCam}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Adoption Date</label>
+                    <input type="number" className="form-control" id="age"  value = {this.state.adoption_date} onChange = {this.setAdoptionDate}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Animal Id</label>
+                    <input type="number" className="form-control" id="age"  value = {this.state.animal_id} onChange = {this.setAnimalId}/>
+                    
+                </div>
+                <div className="form-group">
+                    <label for="age">Member Id</label>
+                    <input type="number" className="form-control" id="age" value = {this.state.member_id} onChange = {this.setMemberId}/>
+                    
+                </div>
+              <button className="btn btn-primary" onClick = {this.saveAdoption}>Update</button>
+        
+        </div>
+        );
+    }
 }
+
+export default UpdateEmployee;
