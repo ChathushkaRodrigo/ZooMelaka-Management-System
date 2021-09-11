d99865e1c742550aed0683fe163b8b25190e7036
shas99 <pshasvathan1999@gmail.com>
2021-09-11 17:27:55 +0530

    made footer black

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function footer() {
     return (
         <div>
-            <div className="fixed-footer" style={{position:'fixed',bottom:0,width:'100%',height:'80px',backgroundColor:'#142e1a',opacity:"0.9"}}>
+            <div className="fixed-footer" style={{position:'fixed',bottom:0,width:'100%',height:'80px',backgroundColor:'black',opacity:"0.8"}}>
                <b> <p style={{color:'white'}}>Made by Curtin Undergraduates batch of 2023</p>
                 <p style={{color:'white'}}>Contact us: +94769895299</p></b>
                 <div></div>
