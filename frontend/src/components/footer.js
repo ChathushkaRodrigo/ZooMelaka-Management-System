82bc57ee3235902782cd154d024052c5f9cd10b0
Randimal Fernando <62325915+randimalfernando@users.noreply.github.com>
2021-09-14 12:31:18 +0530

    footer updated

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,6 +3,12 @@ import "../CSS/App.css"
 export default function footer() {
     return (
         <div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <div className="fixed-footer" style={{bottom:0,width:'100%',height:'80px'}}>
                <b> <p style={{color:'white'}}>Made by Curtin Undergraduates batch of 2023</p>
                 <p style={{color:'white'}}>Contact us: +94769895299</p></b>
